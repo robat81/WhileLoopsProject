ace128b094d9845538a22c356af9b784c2b4dd8a
unknown <fesst506@CE105WD554110S.hillnet.ad.hsd.k12.or.us>
2015-02-12 09:48:27 -0800

    yup

---
+++ Rock Paper Scissors Lizard Spock.py
@@ -0,0 +1,25 @@
+#Rock Paper Scissors Lizard Spock
+#Tabor Fessenden
+
+import random
+from tkinter import *
+import tkinter.simpledialog as simpledialog
+import tkinter.messagebox as messagebox
+
+#Assignment
+rock = 0
+Spock = 1
+paper = 2
+lizard = 3
+scissors = 4
+
+messagebox.showinfo("Welcome",
+                    "We're going to play Rock, Paper," + \
+                    " Scissors, Lizard, Spock! \n " + \
+                    "You choose one of the 5 abilities " + \
+                    "and go up against this program.")
+
+user = simpledialog.askinteger("Choose your ability.",
+                               "Enter 0 for rock, 1 for Spock, " +\
+                               "2 for paper, 3 for lizard " +\
+                               "and 4 for scissors.")
